ab1db5579e1a97bf02623ef1877d6cf7f4f666ab
Brad Culwell <bradculwell@pm.me>
2024-03-14 13:54:47 -0500

    Remove escaping from command substitutions

--- src/client/cmd.rs
+++ src/client/cmd.rs
@@ -3,55 +3,45 @@ use std::{
     process::{Command, Stdio},
 };
 
-use crate::{app::App, source::Item};
+use crate::{app::App, catch, source::Item};
 
 pub fn load_config(app: &mut App) {
     if app.config.torrent_client_cmd.is_none() {
         app.config.torrent_client_cmd = Some(
             #[cfg(windows)]
-            "curl {torrent} -o ~\\Downloads\\{file}".to_owned(),
+            "curl \"{torrent}\" -o ~\\Downloads\\{file}".to_owned(),
             #[cfg(unix)]
-            "curl {torrent} > ~/{file}".to_owned(),
+            "curl \"{torrent}\" > ~/{file}".to_owned(),
         );
     }
 }
 
 pub async fn download(item: &Item, app: &mut App) {
     let cmd = app.config.torrent_client_cmd.clone().unwrap_or_default();
+    let cmd_str = cmd
+        .replace("{magnet}", &item.magnet_link)
+        .replace("{torrent}", &item.torrent_link)
+        .replace("{title}", &item.title)
+        .replace("{file}", &item.file_name);
+
+    // Use platform-specific shell to launch command
     #[cfg(target_os = "windows")]
-    let (cmd_str, cmd) = {
-        let cmd_str = cmd
-            .replace("{magnet}", &item.magnet_link)
-            .replace("{torrent}", &item.torrent_link)
-            .replace("{title}", &item.title)
-            .replace("{file}", &item.file_name);
-
-        let cmd = Command::new("powershell.exe")
-            .arg("-Command")
-            .arg(&cmd_str)
-            .stdin(Stdio::null())
-            .stdout(Stdio::null())
-            .stderr(Stdio::piped())
-            .spawn();
-        (cmd_str, cmd)
-    };
+    let cmd = Command::new("powershell.exe")
+        .arg("-Command")
+        .arg(&cmd_str)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::piped())
+        .spawn();
     #[cfg(not(target_os = "windows"))]
-    let (cmd_str, cmd) = {
-        let cmd_str = cmd
-            .replace("{magnet}", &shellwords::escape(&item.magnet_link))
-            .replace("{torrent}", &shellwords::escape(&item.torrent_link))
-            .replace("{title}", &shellwords::escape(&item.title))
-            .replace("{file}", &shellwords::escape(&item.file_name));
+    let cmd = Command::new("sh")
+        .arg("-c")
+        .arg(&cmd_str)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::piped())
+        .spawn();
 
-        let cmd = Command::new("sh")
-            .arg("-c")
-            .arg(&cmd_str)
-            .stdin(Stdio::null())
-            .stdout(Stdio::null())
-            .stderr(Stdio::piped())
-            .spawn();
-        (cmd_str, cmd)
-    };
     let child = match cmd {
         Ok(child) => child,
         Err(e) => {
