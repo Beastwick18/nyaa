3b3756e8117fd66768d36002bedf2095654bb0a6
Chris Benz <me@chrisbenz.dev>
2024-03-29 21:08:14 -0300

    Removed unused import warning since ClipboardContext is not really used

--- src/clip.rs
+++ src/clip.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 use cli_clipboard::{
-    ClipboardContext, ClipboardProvider,
+    ClipboardProvider,
 };
 use serde::{Deserialize, Serialize};
 
@@ -58,7 +58,7 @@ pub fn copy_to_clipboard(
     }
     #[cfg(not(target_os = "linux"))]
     {
-        let mut ctx: ClipboardContext = match ClipboardProvider::new() {
+        let mut ctx: ClipboardProvider = match ClipboardProvider::new() {
             Ok(ctx) => ctx,
             Err(e) => {
                 return Err(format!("Failed to copy to clipboard:\n{}", e).into());
