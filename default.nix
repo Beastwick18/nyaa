8e57af644845f25469b02c57257a22a350f09d94
Brad Culwell <bradculwell@pm.me>
2024-06-08 13:00:28 -0500

    fix(nix): add git hash for transmission-rpc

--- default.nix
+++ default.nix
@@ -2,6 +2,11 @@
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "nyaa";
   version = "0.1";
-  cargoLock.lockFile = ./Cargo.lock;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "transmission-rpc-0.4.2" = "sha256-3na/LUlcCk1MTbFXMtugK8YiLMdwF3m8QnpSKl3B4I8=";
+    };
+  };
   src = pkgs.lib.cleanSource ./.;
 }
