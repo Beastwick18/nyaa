a7cb7755ffd7d12f1fa7afbac2d95e975323a478
Brad Culwell <bradculwell@pm.me>
2024-06-18 06:42:28 +0000

    nix: simplify to just flake.nix

--- flake.nix
+++ flake.nix
@@ -2,21 +2,38 @@
   description = "A nyaa.si tui tool for browsing and downloading torrents";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		naersk.url = "github:nix-community/naersk";
   };
   outputs = {
     self,
     nixpkgs,
+		naersk,
   }@inputs: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgsFor = nixpkgs.legacyPackages;
+		system = "x86_64-linux";
+		pkgs = import nixpkgs {
+			inherit system;
+		};
+		naersk' = pkgs.callPackage naersk {};
   in {
     packages = forAllSystems (system: {
-      default = pkgsFor.${system}.callPackage ./default.nix {};
+			default = naersk'.buildPackage {
+				name = "nyaa";
+				version =
+					(builtins.fromTOML (
+						builtins.readFile ./Cargo.toml
+					))
+					.package
+					.version;
+				src = pkgs.lib.cleanSource ./.;
+			};
     });
 
     devShells = forAllSystems (system: {
-      default = pkgsFor.${system}.callPackage ./shell.nix {};
+			default = pkgs.mkShell {
+				nativeBuildInputs = with pkgs; [ rust.packages.stable.rustPlatform.rustcSrc rustc cargo rustPackages.clippy ];
+			};
     });
 
     homeManagerModule = import ./modules/home-manager.nix inputs;
