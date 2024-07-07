24a7a6c1a858215eb2a59c2f2361ae5cab415bb7
Brad Culwell <bradculwell@pm.me>
2024-07-07 18:12:55 +0000

    style(nix-hm): retab

--- modules/clients/rqbit.nix
+++ modules/clients/rqbit.nix
@@ -1,33 +1,33 @@
 { lib, ... }:
 {
-  options.programs.nyaa.client.rqbit = {
-    base_url = lib.mkOption {
-      type = lib.types.str;
-      default = "http://localhost:3030";
-      description = ''
-        The base url for rqbit
-      '';
-    };
-    use_magnet = lib.mkOption {
-      type = lib.types.nullOr lib.types.bool;
-      default = null;
-      description = ''
-        Whether to send magnet links or torrent links (optional)
-      '';
-    };
-    overwrite = lib.mkOption {
-      type = lib.types.nullOr lib.types.bool;
-      default = null;
-      description = ''
-        Whether to overwrite existing files while downloading (optional)
-      '';
-    };
-    output_folder = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = ''
-        The path to save downloaded torrents to (optional)
-      '';
-    };
-  };
+	options.programs.nyaa.client.rqbit = {
+		base_url = lib.mkOption {
+			type = lib.types.str;
+			default = "http://localhost:3030";
+			description = ''
+				The base url for rqbit
+			'';
+		};
+		use_magnet = lib.mkOption {
+			type = lib.types.nullOr lib.types.bool;
+			default = null;
+			description = ''
+				Whether to send magnet links or torrent links (optional)
+			'';
+		};
+		overwrite = lib.mkOption {
+			type = lib.types.nullOr lib.types.bool;
+			default = null;
+			description = ''
+				Whether to overwrite existing files while downloading (optional)
+			'';
+		};
+		output_folder = lib.mkOption {
+			type = lib.types.nullOr lib.types.str;
+			default = null;
+			description = ''
+				The path to save downloaded torrents to (optional)
+			'';
+		};
+	};
 }
--- modules/sources/nyaa.nix
+++ modules/sources/nyaa.nix
@@ -1,113 +1,113 @@
 { lib, ... }:
 {
-  options.programs.nyaa.source.nyaa = {
-    base_url = lib.mkOption {
-      type = lib.types.str;
-      default = "https://nyaa.si";
-      description = ''
-        The base url for Nyaa
-      '';
-    };
-    default_sort = lib.mkOption {
-      type = lib.types.str;
-      default = "Date";
-      description = ''
-        The default sort for nyaa
-      '';
-    };
-    default_sort_dir = lib.mkOption {
-      type = lib.types.str;
-      default = "Desc";
-      description = ''
-        The default sort direction for nyaa
-      '';
-    };
-    default_filter = lib.mkOption {
-      type = lib.types.str;
-      default = "NoFilter";
-      description = ''
-        The default filter for nyaa
-      '';
-    };
-    default_category = lib.mkOption {
-      type = lib.types.str;
-      default = "AllCategories";
-      description = ''
-        The default category for nyaa
-      '';
-    };
-    default_search = lib.mkOption {
-      type = lib.types.str;
-      default = "";
-      description = ''
-        The default search for nyaa
-      '';
-    };
-    rss = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = ''
-        Whether to use rss for search
-      '';
-    };
-    timeout = lib.mkOption {
-      type = lib.types.nullOr lib.types.int;
-      default = null;
-      description = ''
-        Override the global timeout for this source (measured in seconds) (optional)
-      '';
-    };
+	options.programs.nyaa.source.nyaa = {
+		base_url = lib.mkOption {
+			type = lib.types.str;
+			default = "https://nyaa.si";
+			description = ''
+				The base url for Nyaa
+			'';
+		};
+		default_sort = lib.mkOption {
+			type = lib.types.str;
+			default = "Date";
+			description = ''
+				The default sort for nyaa
+			'';
+		};
+		default_sort_dir = lib.mkOption {
+			type = lib.types.str;
+			default = "Desc";
+			description = ''
+				The default sort direction for nyaa
+			'';
+		};
+		default_filter = lib.mkOption {
+			type = lib.types.str;
+			default = "NoFilter";
+			description = ''
+				The default filter for nyaa
+			'';
+		};
+		default_category = lib.mkOption {
+			type = lib.types.str;
+			default = "AllCategories";
+			description = ''
+				The default category for nyaa
+			'';
+		};
+		default_search = lib.mkOption {
+			type = lib.types.str;
+			default = "";
+			description = ''
+				The default search for nyaa
+			'';
+		};
+		rss = lib.mkOption {
+			type = lib.types.bool;
+			default = false;
+			description = ''
+				Whether to use rss for search
+			'';
+		};
+		timeout = lib.mkOption {
+			type = lib.types.nullOr lib.types.int;
+			default = null;
+			description = ''
+				Override the global timeout for this source (measured in seconds) (optional)
+			'';
+		};
 
-    columns = {
-      category = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Whether to enable the category column (optional)
-        '';
-      };
-      title = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Whether to enable the title column (optional)
-        '';
-      };
-      size = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Whether to enable the size column (optional)
-        '';
-      };
-      date = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Whether to enable the date column (optional)
-        '';
-      };
-      seeders = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Whether to enable the seeders column (optional)
-        '';
-      };
-      leechers = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Whether to enable the leechers column (optional)
-        '';
-      };
-      downloads = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Whether to enable the downloads column (optional)
-        '';
-      };
-    };
-  };
+		columns = {
+			category = lib.mkOption {
+				type = lib.types.nullOr lib.types.bool;
+				default = null;
+				description = ''
+					Whether to enable the category column (optional)
+				'';
+			};
+			title = lib.mkOption {
+				type = lib.types.nullOr lib.types.bool;
+				default = null;
+				description = ''
+					Whether to enable the title column (optional)
+				'';
+			};
+			size = lib.mkOption {
+				type = lib.types.nullOr lib.types.bool;
+				default = null;
+				description = ''
+					Whether to enable the size column (optional)
+				'';
+			};
+			date = lib.mkOption {
+				type = lib.types.nullOr lib.types.bool;
+				default = null;
+				description = ''
+					Whether to enable the date column (optional)
+				'';
+			};
+			seeders = lib.mkOption {
+				type = lib.types.nullOr lib.types.bool;
+				default = null;
+				description = ''
+					Whether to enable the seeders column (optional)
+				'';
+			};
+			leechers = lib.mkOption {
+				type = lib.types.nullOr lib.types.bool;
+				default = null;
+				description = ''
+					Whether to enable the leechers column (optional)
+				'';
+			};
+			downloads = lib.mkOption {
+				type = lib.types.nullOr lib.types.bool;
+				default = null;
+				description = ''
+					Whether to enable the downloads column (optional)
+				'';
+			};
+		};
+	};
 }
